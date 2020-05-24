79bbe339abf25c6282a9e7b49229d8ee84a81630
MARIANACECILIAMATOS <matosm@ar.ibm.com>
2020-05-24 19:07:18 -0300

    Punto 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,7 @@
 module Lib where
 
+import Text.Show.Functions
+
 -- Modelo inicial
 
 data Jugador = UnJugador {
@@ -40,3 +42,40 @@ mayorSegun f a b
 ----------------------------------------------
 ---- Resolución del ejercicio
 ----------------------------------------------
+
+{-
+    a. Modelar los palos usados en el juego que a partir de una determinada habilidad generan un tiro que se compone por velocidad, precisión y altura.
+
+    - El putter genera un tiro con velocidad igual a 10, el doble de la precisión recibida y altura 0.
+    - La madera genera uno de velocidad igual a 100, altura igual a 5 y la mitad de la precisión.
+    - Los hierros, que varían del 1 al 10 (número al que denominaremos n), generan un tiro de velocidad igual a la fuerza multiplicada por n, la precisión dividida por n y una altura de n-3 (con mínimo 0). Modelarlos de la forma más genérica posible.
+-}
+
+type Palo = Habilidad -> Tiro
+
+putter :: Palo
+putter habilidad = UnTiro {
+  velocidad = 10,
+  precision = precisionJugador habilidad * 2,
+  altura = 0
+  }
+madera :: Palo
+madera habilidad = UnTiro {
+  velocidad = 100,
+  precision = precisionJugador habilidad `div` 2,
+  altura = 5
+  }
+
+-- Los hierros, que varían del 1 al 10 (número al que denominaremos n), generan un tiro de velocidad igual a la fuerza multiplicada por n, la precisión dividida por n y una altura de n-3 (con mínimo 0). Modelarlos de la forma más genérica posible.
+hierro :: Int -> Palo
+hierro n habilidad = UnTiro {
+  velocidad = fuerzaJugador habilidad * n,
+  precision = precisionJugador habilidad `div` n,
+  altura = (n - 3) `max` 0
+}
+{-
+    b. Definir una constante palos que sea una lista con todos los palos que se pueden usar en el juego.
+-}
+
+palos :: [Palo]
+palos = [putter , madera] ++ map hierro [1..10]
