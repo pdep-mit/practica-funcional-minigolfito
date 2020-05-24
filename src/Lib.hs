8269aaa1daf4319641b9886082cdda8c05b90d5a
MARIANACECILIAMATOS <matosm@ar.ibm.com>
2020-05-24 18:17:56 -0300

    Agrego código inicial

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,42 @@
 module Lib where
 
-laVerdad = True
+-- Modelo inicial
+
+data Jugador = UnJugador {
+  nombre :: String,
+  padre :: String,
+  habilidad :: Habilidad
+} deriving (Eq, Show)
+
+data Habilidad = Habilidad {
+  fuerzaJugador :: Int,
+  precisionJugador :: Int
+} deriving (Eq, Show)
+
+-- Jugadores de ejemplo
+
+bart = UnJugador "Bart" "Homero" (Habilidad 25 60)
+todd = UnJugador "Todd" "Ned" (Habilidad 15 80)
+rafa = UnJugador "Rafa" "Gorgory" (Habilidad 10 1)
+
+data Tiro = UnTiro {
+  velocidad :: Int,
+  precision :: Int,
+  altura :: Int
+} deriving (Eq, Show)
+
+type Puntos = Int
+
+-- Funciones útiles
+
+between n m x = elem x [n .. m]
+
+maximoSegun f = foldl1 (mayorSegun f)
+
+mayorSegun f a b
+  | f a > f b = a
+  | otherwise = b
+
+----------------------------------------------
+---- Resolución del ejercicio
+----------------------------------------------
